c66a7b335c24436ca76ae77bd369df002eccd453
Evgeniy Peshkov <e.peshkov@dodopizza.com>
2019-09-01 11:32:44 +0300

    start

---
+++ src/test/kotlin/tests.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+@Test fun EqualArrayShoudReturnZeroZero(){
+    val array = intArrayOf(1,2,3).toTypedArray()
+    val result = compareTriplets(array, array)
+
+
+
+}
