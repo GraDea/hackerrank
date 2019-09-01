53e4bba0b7872f3a616ce60f0f169d5e4dfa6d55
Evgeniy Peshkov <e.peshkov@dodopizza.com>
2019-09-01 12:24:33 +0300

    compare tripltes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -20,7 +20,18 @@ import kotlin.text.*
 // Complete the compareTriplets function below.
 fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
 
-    TODO()
+    var first = 0
+    var second = 0
+
+    for(i in 0..2)
+    {
+        if (a[i]>b[i]){
+            first++
+        } else if (a[i]<b[i]){
+            second++
+        }
+    }
+    return intArrayOf(first, second).toTypedArray()
 }
 
 fun main(args: Array<String>) {
--- src/test/kotlin/tests.kt
+++ src/test/kotlin/tests.kt
@@ -1,6 +1,35 @@
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should contain same`
+import org.amshove.kluent.should
 import org.junit.jupiter.api.Test
 
-@Test fun EqualArrayShoudReturnZeroZero(){
-    val array = intArrayOf(1,2,3).toTypedArray()
-    val result = compareTriplets(array, array)
+class CompareTripletsTest {
+
+    @Test
+    fun EqualArraysShoudReturnZeroZero() {
+        val array = intArrayOf(1, 2, 3).toTypedArray()
+        val result = compareTriplets(array, array)
+
+        result `should contain same` count("00")
+    }
+
+    @Test
+    fun FirstAllGreaterShouldReturnThreeZero() {
+        val array1 = intArrayOf(5, 2, 3).toTypedArray()
+        val array2 = intArrayOf(0, 0, 0).toTypedArray()
+        val result = compareTriplets(array1, array2)
+
+        result `should contain same` count("30")
+    }
+
+    @Test
+    fun SecondAllGreaterShouldReturnThreeZero() {
+        val array1 = intArrayOf(0, 0, 0).toTypedArray()
+        val array2 = intArrayOf(5, 2, 3).toTypedArray()
+        val result = compareTriplets(array1, array2)
+
+        result `should contain same` count("03")
+    }
+
+    private fun count(count: String) : Array<Int> = intArrayOf(count[0].toString().toInt(), count[1].toString().toInt()).toTypedArray()
 }
